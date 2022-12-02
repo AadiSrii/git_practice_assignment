67c4d99f49c5c1d43bb9d3503b66dd8bf6e453d4
bernard powers <adarshsrivastava0348@gmail.com>
2022-12-02 15:43:45 +0530

    check the code

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+   let bag="";
+    let bag1=""+num;
+  let N=num;
+   
+    for(let i=bag1.length-1;i>=0;i--)
+    {
+        bag=bag+bag1[i];
+    }
+    if(bag1=bag){
+    console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
